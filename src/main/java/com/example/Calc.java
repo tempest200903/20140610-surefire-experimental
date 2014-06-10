3a3c406b5add889967c044d457fa653ca77d4755
tempest200903 <tempest200903@gmail.com>
2014-06-11 00:04:29 +0900

    first commit

---
+++ src/main/java/com/example/Calc.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Calc {
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/example/CalcTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalcTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		String option = System.getProperty("option");
+		System.out.println("option =: " + option);
+		fail("Not yet implemented");
+	}
+
+}
